3bf6e843bb4fd06120c7be270c06b6f48e544bb5
locbui0311 <locbui.it.0311@gmail.com>
2021-07-16 23:27:40 +0700

    Init Project

---
+++ src/quanli_cafe/QuanLi_CaFe.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanli_cafe;
+
+/**
+ *
+ * @author LocBui
+ */
+public class QuanLi_CaFe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        fLogin frm= new fLogin(); 
+        frm.setLocationRelativeTo(null); //đưa form ra giữa màn hình
+        frm.setVisible(true);
+    }
+    
+}
